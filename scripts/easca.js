2ea782a703a305a5b84795e417cb975988e39512
ribo <roibeard.mac.unfraidh@gmail.com>
2019-11-13 20:27:42 +0100

    Init easca git repo.

---
+++ scripts/easca.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {
+	$('#alt-keyboard-container').hide();
+	$('#caps-keyboard-container').hide();
+
+	let fadaMode=false;
+	let capsMode=false;
+
+toggleCapsMode = ()=>{
+if (capsMode===true){
+	$('#keyboard-container').show();
+	$('#caps-keyboard-container').hide();
+	capsMode=false;
+}
+else{
+		$('#keyboard-container').hide();
+		$('#alt-keyboard-container').hide();
+	$('#caps-keyboard-container').show();
+
+	capsMode=true;
+
+}
+;
+
+
+}
+	toggleKeyboard = ()=>{
+if (fadaMode===false){
+	$('#keyboard-container').hide();
+	$('#alt-keyboard-container').show();
+	// $('#keyboard-container').show();
+	// $('#alt-keyboard-container').hide();
+	fadaMode=true;	
+}
+	else{
+		$('#keyboard-container').show();
+	$('#alt-keyboard-container').hide();
+	fadaMode=false;
+	}
+}
+
+	let keyPressed;
+		$('.btn').click(function(){
+			var id = this.id
+			keyPressed = (this.innerHTML);
+			if (keyPressed=== "fadaí"){
+				toggleKeyboard();
+				// alert('alt')
+			}
+			else if(
+				
+				(this.id === "caps")
+			){
+				toggleCapsMode()
+
+			}
+			else
+			$('#output').append(keyPressed)
+			if (id ==='return'){
+				$('#output').empty();
+			
+			}
+			// if (id === 'undo'){
+			// 	let temp = document.getElementById('#output');
+			// 	alert(temp)
+			// }
+		});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
