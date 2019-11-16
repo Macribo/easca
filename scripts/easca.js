a4af78a80deb8b52e0357a538c5b22a08462dfea
ribo <roibeard.mac.unfraidh@gmail.com>
2019-11-16 14:41:01 +0100

    show key options on hold keypress.

--- scripts/easca.js
+++ scripts/easca.js
@@ -1,4 +1,110 @@
 $(document).ready(function () {
+	let keyPressed;
+
+	let keysToShow = [];
+	showWhatKeys=(keyPressed)=>{
+		switch(keyPressed){
+			case 'a': keysToShow.push('a');
+					keysToShow.push('á');
+					keysToShow.push('A');
+					break;
+			
+
+					case 'b': keysToShow.push('b');
+					keysToShow.push('ḃ');
+					keysToShow.push('B');
+					break;
+			
+					case 'c': keysToShow.push('c');
+					keysToShow.push('ċ');
+					keysToShow.push('C');
+					break;
+			
+					case 'd': keysToShow.push('d');
+					keysToShow.push('ḋ');
+					keysToShow.push('D');
+					break;
+			
+					case 'e': keysToShow.push('a');
+					keysToShow.push('á');
+					keysToShow.push('A');
+					break;
+			
+					case 'f': keysToShow.push('f');
+					keysToShow.push('ḟ');
+					keysToShow.push('F');
+					break;
+			
+					case 'g': keysToShow.push('g');
+					keysToShow.push('ġ');
+					keysToShow.push('G');
+					break;
+			
+					case 'h': keysToShow.push('h');
+					keysToShow.push('H');
+					break;
+			
+					case 'i': keysToShow.push('i');
+					keysToShow.push('í');
+					keysToShow.push('I');
+					break;
+			
+					case 'l': keysToShow.push('l');
+					keysToShow.push('L');
+					break;
+			
+					case 'm': keysToShow.push('m');
+					keysToShow.push('ṁ');
+					keysToShow.push('M');
+					break;
+			
+					case 'n': keysToShow.push('n');
+					keysToShow.push('N');
+					break;
+			
+					case 'o': keysToShow.push('o');
+					keysToShow.push('ó');
+					keysToShow.push('O');
+					break;
+			
+					case 'p': keysToShow.push('p');
+					keysToShow.push('ṗ');
+					keysToShow.push('P');
+					break;
+			
+					case 'r': keysToShow.push('r');
+					keysToShow.push('ꞃ');
+					keysToShow.push('R');
+					break;
+			
+					case 's': keysToShow.push('s');
+					keysToShow.push('ṡ');
+					keysToShow.push('S');
+					break;
+			
+					case 't': keysToShow.push('t');
+					keysToShow.push('ṫ');
+					keysToShow.push('T');
+					break;
+			
+					case 'u': keysToShow.push('u');
+					keysToShow.push('ú');
+					keysToShow.push('U');
+					break;
+			
+			
+
+			default: break;
+		}
+	
+	}
+	
+	
+	
+	
+	
+
+
 	$('#fada-mode').hide();
 	$('#caps-keyboard-container').hide();
 capsMode=false;
@@ -20,7 +126,6 @@ else{
 }
 ;
 
-
 }
 	fadaMode = ()=>{
 
@@ -39,18 +144,41 @@ else{
 // 	fadaMode=false;
 // 	}
 // }
-function myFunction() {
-	alert("Hello");
-  }
-	let keyPressed;
+function holdKeyDown() {
+	// alert(keyPressed);
+	
+	// alert(keysToShow)
+	for (let i = 0; i<keysToShow.length; i++){
+		let newButton = `<button type="button" class="btn btn-right-logo btn-outline-dark">`+keysToShow[i]+`</button>`
+		
+		$('#j-line').append(newButton)
+	}
+
+	$('#j-line').fadeIn();
+	let newLeft = $('#'+keyPressed).offset().left;
+	let newTop = $('#'+keyPressed).offset().top -50;
+	$('#j-line').css({"left": newLeft});
+	$('#j-line').css({"top": newTop});
+	setTimeout(function(){
+		$('#j-line').fadeOut();
+		// alert("removed");
+},2000
+)
+}
 	$('.btn').on('touchstart', function(){
-		myVar = setTimeout(myFunction, 3000)
+		$('#j-line').empty();
+		keyPressed = (this.innerHTML);
+		keysToShow=[];
+
+		showWhatKeys(keyPressed);
+
+		myVar = setTimeout(holdKeyDown, 1200);
+		console.log(event.clientX, event.clientY)
 	
 	})
 		$('.btn').on( 'touchend' , function(e) {
 			clearTimeout(myVar)
 				var id = this.id
-				keyPressed = (this.innerHTML);
 				if (keyPressed=== "Ᵹ"){
 					fadaMode();
 					// alert('alt')
@@ -68,18 +196,14 @@ function myFunction() {
 					$('#output').empty();
 				
 				}
-		});
+		keysToShow= [];
+	
+			});
 			// if (id === 'undo'){
 			// 	let temp = document.getElementById('#output');
 			// 	alert(temp)
 			// }
 		});
-
-
-
-
-
-
 
 
 
