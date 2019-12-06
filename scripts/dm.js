730bd07a54da796c544166fc171ab86552978c88
macribo <roibeard.mac.unfraidh@gmail.com>
2019-12-06 11:31:56 +0100

    Tosaigh ar modúl dungeon master

---
+++ scripts/dm.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+
+    /*update keyboard appearance
+    according to dm is speaking or not.
+    
+
+
+    player-can-type = false;
+
+    listen for green keypress
+
+    */
+
+
+   let dmAgCainnt = true;
+
+   if(dmAgCainnt){
+
+$('.dm-says').html(`Do what is beyond your strength even should you fail sometimes."
+<br>Charles Stewart Parnell`
+) 
+
+   };
+
+});
